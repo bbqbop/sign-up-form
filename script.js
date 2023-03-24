ab78d833cbb70ced0c90fa21d8baacd5e9fb424b
bbqbop <dierk.peters@gmail.com>
2023-03-23 23:14:50 -0400

    attempt to validate with js, needs work

--- script.js
+++ script.js
@@ -2,9 +2,19 @@ const pwd = document.querySelector("#pwd");
 const pwdConfirm = document.querySelector("#cpwd");
 const pwdLabel = document.querySelector('#pwd-label')
 
-document.addEventListener('submit', (e)=> {
+const labels = document.querySelectorAll('label');
+const inputs = document.querySelectorAll('input');
+const submit = document.querySelector('button');
+
+submit.addEventListener('click', (e)=>{
     e.preventDefault();
-    if(pwd.value != pwdConfirm.value){
-        pwdLabel.style
+    if(inputs[4].value !== inputs[5].value){
+        console.log('sss');
+        let before = document.createElement('p');
+        before.classList.add("correct");
+        before.textContent = inputs[5].dataset.correct;
+        labels[5].append(before);
+        console.log(before);
     }
-})
+    }
+    )
