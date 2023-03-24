51644f6dfb28f5d97d4b535472f573ff77e1552e
bbqbop <dierk.peters@gmail.com>
2023-03-23 22:21:14 -0400

    css colors for validation

---
+++ script.js
@@ -0,0 +1,10 @@
+const pwd = document.querySelector("#pwd");
+const pwdConfirm = document.querySelector("#cpwd");
+const pwdLabel = document.querySelector('#pwd-label')
+
+document.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    if(pwd.value != pwdConfirm.value){
+        pwdLabel.style
+    }
+})
