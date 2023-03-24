fc9f2914132edee0c04006f916ab206f8645bdfe
bbqbop <dierk.peters@gmail.com>
2023-03-24 12:15:53 -0400

    add js to validate and show correction

--- script.js
+++ script.js
@@ -6,15 +6,40 @@ const labels = document.querySelectorAll('label');
 const inputs = document.querySelectorAll('input');
 const submit = document.querySelector('button');
 
-submit.addEventListener('click', (e)=>{
-    e.preventDefault();
-    if(inputs[4].value !== inputs[5].value){
-        console.log('sss');
+// input validation:
+for(i=0; i < inputs.length; i++){
+    let input = inputs[i];
+    let idx = i;
+    input.addEventListener('keydown', (e)=>{
+        // confirm passwords:
+        if(idx === 5){
+            if(input.value != inputs[4].value){
+                input.setCustomValidity("Invalid field.");
+                createCorrect(input, idx);
+            } else {
+                input.setCustomValidity("");
+                eraseCorrect(idx);
+            }
+        }
+        // validate other inputs:
+        let isValid = input.checkValidity();
+        if(!isValid){
+            createCorrect(input, idx);
+        } else {
+            eraseCorrect(idx);
+        }
+    })
+}
+function createCorrect(input, idx){
+    if(labels[idx].childElementCount === 1){
         let before = document.createElement('p');
+        before.textContent = input.dataset.correct;
+        labels[idx].append(before);
         before.classList.add("correct");
-        before.textContent = inputs[5].dataset.correct;
-        labels[5].append(before);
-        console.log(before);
     }
+}
+function eraseCorrect(idx){
+    if(labels[idx].childElementCount != 1){
+        labels[idx].lastChild.remove();
     }
-    )
+}
